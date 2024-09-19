a18c216bd75e05e9c5ea2640b324e2ac0589cc30
Daniel Errington <derrington@gmail.com>
2024-09-20 00:11:31 +0200

    Implement game clock

--- amethyst/Services/GameClock.cs
+++ amethyst/Services/GameClock.cs
@@ -2,13 +2,14 @@
 
 public interface IGameClock : IDisposable
 {
+    void Run();
 }
 
-public class GameClock(IEnumerable<ITickReceiver> receivers) : IGameClock
+public class GameClock(IEnumerable<ITickReceiver> receivers, ILogger<GameClock> logger) : IGameClock
 {
     public int MillisecondsBetweenFrames { get; init; } = 100;
 
-    private volatile bool _isRunning = false;
+    private volatile bool _isRunning;
 
     public void Run()
     {
@@ -16,11 +17,34 @@ public class GameClock(IEnumerable<ITickReceiver> receivers) : IGameClock
 
         new Thread(() =>
         {
+            var lastTick = GetTick();
+
             while (_isRunning)
             {
+                var tick = GetTick();
+                var tickDelta = tick - lastTick;
 
+                foreach (var receiver in receivers)
+                {
+                    try
+                    {
+                        receiver.Tick(tick, tickDelta);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "Error while processing tick with receiver {receiverName}", receiver.GetType().Name);
+                    }
+                }
+
+                lastTick = tick;
+
+                Thread.Sleep(MillisecondsBetweenFrames);
             }
         }).Start();
+
+        return;
+
+        static long GetTick() => DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
     }
 
     public void Dispose()
