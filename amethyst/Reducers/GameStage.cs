37e451ccef0b68a2349deb7853c9420f49820b2e
Daniel Errington <daniel.errington@blueprism.com>
2024-09-30 22:19:39 +0100

    Add start of game stage reducer

---
+++ amethyst/Reducers/GameStage.cs
@@ -0,0 +1,36 @@
+﻿using amethyst.Events;
+using amethyst.Services;
+
+namespace amethyst.Reducers;
+
+public class GameStage(GameContext context) : Reducer<GameStageState>(context), IHandlesEvent<JamStarted>
+    , IHandlesEvent<JamEnded>
+    , IHandlesEvent<TimeoutStarted>
+{
+    protected override GameStageState DefaultState => new(Stage.BeforeGame);
+
+    public void Handle(JamStarted @event) =>
+        SetState(new(Stage.Jam));
+
+    public void Handle(JamEnded @event)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Handle(TimeoutStarted @event)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public record GameStageState(Stage Stage);
+
+public enum Stage
+{
+    BeforeGame,
+    Lineup,
+    Jam,
+    Timeout,
+    Intermission,
+    AfterGame,
+}
