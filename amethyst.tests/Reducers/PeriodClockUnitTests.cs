96f2681ddd2c90df13d521205f5a93d27da6c6f0
Daniel Errington <daniel.errington@blueprism.com>
2024-09-30 22:19:13 +0100

    Add period clock and start of associated unit tests

---
+++ amethyst.tests/Reducers/PeriodClockUnitTests.cs
@@ -0,0 +1,43 @@
+﻿using amethyst.Events;
+using amethyst.Reducers;
+using amethyst.Services;
+using FluentAssertions;
+using Moq;
+
+using static amethyst.tests.DataGenerator;
+
+namespace amethyst.tests.Reducers;
+
+public class PeriodClockUnitTests : UnitTest<PeriodClock>
+{
+    private PeriodClockState _state;
+
+    protected override void Setup()
+    {
+        base.Setup();
+
+        _state = (PeriodClockState)Subject.GetDefaultState();
+
+        GetMock<IGameStateStore>()
+            .Setup(mock => mock.GetState<PeriodClockState>())
+            .Returns(() => _state);
+
+        GetMock<IGameStateStore>()
+            .Setup(mock => mock.SetState(It.IsAny<PeriodClockState>()))
+            .Callback((PeriodClockState s) => _state = s);
+    }
+
+    [Test]
+    public void JamStart_WhenPeriodClockStopped_StartsPeriod()
+    {
+        var randomTick = GetRandomTick();
+
+        Subject.Handle(new JamStarted(randomTick));
+
+        _state.IsRunning.Should().BeTrue();
+        _state.LastStartTick.Should().Be(randomTick);
+        _state.TicksPassed.Should().Be(0);
+        _state.SecondsPassed.Should().Be(0);
+        _state.TicksPassedAtLastStart.Should().Be(0);
+    }
+}
---
+++ amethyst/Reducers/PeriodClock.cs
@@ -0,0 +1,103 @@
+﻿using amethyst.Events;
+using amethyst.Services;
+
+namespace amethyst.Reducers;
+
+public class PeriodClock(GameContext context, ILogger<PeriodClock> logger) 
+    : Reducer<PeriodClockState>(context)
+    , IHandlesEvent<JamStarted>
+    , IHandlesEvent<JamEnded>
+    , IHandlesEvent<TimeoutStarted>
+    , IHandlesEvent<TimeoutEnded>
+    , ITickReceiver
+{
+    protected override PeriodClockState DefaultState => new(false, 0, 0, 0, 0);
+
+    private const long PeriodLengthInTicks = 30 * 60 * 1000;
+    private const long LineupDurationInTicks = 30 * 1000;
+
+    public void Handle(JamStarted @event)
+    {
+        var state = GetState();
+        if (state.IsRunning) return;
+
+        logger.LogDebug("Starting period clock due to jam start");
+
+        SetState(GetState() with
+        {
+            IsRunning = true, 
+            TicksPassedAtLastStart = state.TicksPassed,
+            LastStartTick = @event.Tick,
+        });
+    }
+
+    public void Handle(JamEnded @event)
+    {
+        var state = GetState();
+        if (!state.IsRunning) return;
+
+        var ticksPassed = @event.Tick - state.LastStartTick + state.TicksPassedAtLastStart;
+
+        if (ticksPassed < PeriodLengthInTicks) return;
+
+        logger.LogInformation("Period clock expired following jam end");
+
+        SetState(state with {IsRunning = false, SecondsPassed = (int) (ticksPassed / 1000), TicksPassed = ticksPassed});
+    }
+
+    public void Handle(TimeoutStarted @event)
+    {
+        var state = GetState();
+        if (!state.IsRunning) return;
+
+        logger.LogDebug("Stopping period clock due to timeout start");
+
+        var ticksPassed = Math.Min(PeriodLengthInTicks, @event.Tick - state.LastStartTick + state.TicksPassedAtLastStart);
+
+        SetState(state with
+        {
+            IsRunning = false,
+            SecondsPassed = (int)(ticksPassed / 1000),
+            TicksPassed = ticksPassed,
+        });
+    }
+
+    public void Handle(TimeoutEnded @event)
+    {
+        var state = GetState();
+
+        var ticksRemaining = PeriodLengthInTicks - state.TicksPassed;
+
+        if (ticksRemaining > LineupDurationInTicks) return;
+
+        logger.LogDebug("Starting period clock as timeout ended with less than lineup duration on clock");
+
+        SetState(state with
+        {
+            IsRunning = true,
+            TicksPassedAtLastStart = state.TicksPassed,
+            LastStartTick = @event.Tick,
+        });
+    }
+
+    public void Tick(long tick, long tickDelta)
+    {
+        var state = GetState();
+        if (!state.IsRunning) return;
+
+        var ticksPassed = Math.Min(PeriodLengthInTicks, tick - state.LastStartTick + state.TicksPassedAtLastStart);
+
+        if (ticksPassed == PeriodLengthInTicks)
+        {
+            logger.LogDebug("Period clock expired");
+        }
+
+        SetState(state with
+        {
+            TicksPassed = ticksPassed,
+            SecondsPassed = (int)(ticksPassed / 1000L)
+        });
+    }
+}
+
+public record PeriodClockState(bool IsRunning, long LastStartTick, long TicksPassedAtLastStart, [property: IgnoreChange] long TicksPassed, int SecondsPassed);
