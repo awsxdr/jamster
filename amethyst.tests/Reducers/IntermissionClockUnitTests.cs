be9735cb02df9ce1549988b8931e56b436fb3afe
Daniel Errington <daniel.errington@blueprism.com>
2024-10-03 19:26:01 +0100

    Add intermission clock

---
+++ amethyst.tests/Reducers/IntermissionClockUnitTests.cs
@@ -0,0 +1,76 @@
+﻿using amethyst.Events;
+using amethyst.Reducers;
+using FluentAssertions;
+
+namespace amethyst.tests.Reducers;
+
+public class IntermissionClockUnitTests : ReducerUnitTest<IntermissionClock, IntermissionClockState>
+{
+    [Test]
+    public void JamStarted_WhenClockIsRunning_StopsClock()
+    {
+        State = new(true, false, 20000, 10);
+
+        Subject.Handle(new JamStarted(15000));
+
+        State.IsRunning.Should().BeFalse();
+    }
+
+    [Test]
+    public void JamStarted_WhenClockIsNotRunning_DoesNotChangeState()
+    {
+        State = new(false, false, 20000, 10);
+        var originalState = State;
+
+        Subject.Handle(new JamStarted(15000));
+
+        State.Should().Be(originalState);
+    }
+
+    [Test]
+    public void IntermissionEnded_WhenClockIsRunning_StopsClock()
+    {
+        State = new(true, false, 20000, 10);
+
+        Subject.Handle(new IntermissionEnded(15000));
+
+        State.IsRunning.Should().BeFalse();
+    }
+
+    [Test]
+    public void IntermissionEnded_WhenClockIsNotRunning_DoesNotChangeState()
+    {
+        State = new(false, false, 20000, 10);
+        var originalState = State;
+
+        Subject.Handle(new IntermissionEnded(15000));
+
+        State.Should().Be(originalState);
+    }
+
+    [Test]
+    public void Tick_WhenClockIsRunning_SetsNewTime()
+    {
+        State = new(true, false, 30000, 10);
+
+        Subject.Tick(22000, 2000);
+
+        State.IsRunning.Should().BeTrue();
+        State.HasExpired.Should().BeFalse();
+        State.SecondsRemaining.Should().Be(8);
+        State.TargetTick.Should().Be(30000);
+    }
+
+    [Test]
+    public void Tick_WhenClockIsRunning_AndTargetTickHasPassed_MarksClockAsExpired()
+    {
+        State = new(true, false, 30000, 10);
+
+        Subject.Tick(30001, 10);
+
+        State.IsRunning.Should().BeTrue();
+        State.HasExpired.Should().BeTrue();
+        State.SecondsRemaining.Should().Be(0);
+        State.TargetTick.Should().Be(30000);
+    }
+}
---
+++ amethyst/Reducers/IntermissionClock.cs
@@ -0,0 +1,53 @@
+﻿using amethyst.Domain;
+using amethyst.Events;
+using amethyst.Services;
+
+namespace amethyst.Reducers;
+
+public class IntermissionClock(GameContext context, ILogger<IntermissionClock> logger) 
+    : Reducer<IntermissionClockState>(context)
+    , IHandlesEvent<JamStarted>
+    , IHandlesEvent<IntermissionEnded>
+    , ITickReceiver
+{
+    protected override IntermissionClockState DefaultState => new(false, true, 0, 0);
+
+    public void Handle(JamStarted @event)
+    {
+        var state = GetState();
+
+        if (!state.IsRunning) return;
+
+        SetState(state with { IsRunning = false });
+    }
+
+    public void Handle(IntermissionEnded @event)
+    {
+        var state = GetState();
+
+        if (!state.IsRunning) return;
+
+        SetState(state with { IsRunning = false });
+    }
+
+    public void Tick(Tick tick, long tickDelta)
+    {
+        var state = GetState();
+
+        if (!state.IsRunning) return;
+
+        var ticksRemaining = Math.Max(0, state.TargetTick - tick);
+
+        SetState(state with
+        {
+            HasExpired = ticksRemaining == 0,
+            SecondsRemaining = (int)(ticksRemaining / 1000),
+        });
+    }
+}
+
+public sealed record IntermissionClockState(
+    bool IsRunning,
+    bool HasExpired,
+    Tick TargetTick,
+    int SecondsRemaining);
