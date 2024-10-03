959f8bc403b3a45e1a06f474b8f23226639f9a22
Daniel Errington <daniel.errington@blueprism.com>
2024-10-03 19:25:22 +0100

    Fix Tick equality functions

--- amethyst/Domain/Tick.cs
+++ amethyst/Domain/Tick.cs
@@ -1,39 +1,43 @@
 ﻿namespace amethyst.Domain;
 
-public struct Tick
+public readonly struct Tick(long value)
 {
     public override int GetHashCode()
     {
         return _value.GetHashCode();
     }
 
-    private readonly long _value;
+    private readonly long _value = value switch
+    {
+        > MaxValue => throw new ArgumentException("Value too large", nameof(value)),
+        < MinValue => throw new ArgumentException("Value too small", nameof(value)),
+        _ => value
+    };
 
     public const long MaxValue = long.MaxValue & 0x0000ffffffffffff;
     public const long MinValue = 0;
 
-    public Tick(long value)
-    {
-        if (value > MaxValue) throw new ArgumentException("Value too large", nameof(value));
-        if (value < MinValue) throw new ArgumentException("Value too small", nameof(value));
-
-        _value = value;
-    }
-
     public static implicit operator long(Tick tick) => tick._value;
     public static implicit operator Tick(long tick) => new(tick);
-    public static implicit operator Tick(int tick) => new(tick);
 
     public static bool operator ==(Tick left, Tick right) => left.Equals(right);
     public static bool operator !=(Tick left, Tick right) => !(left == right);
 
-    public bool Equals(Tick other) => _value == other._value;
+    public readonly bool Equals(Tick other) => _value == other._value;
 
-    public override bool Equals(object? obj) =>
+    public readonly override bool Equals(object? obj) =>
         obj switch
         {
             Tick t => Equals(t),
             long l => Equals(l),
+            int i => Equals(i),
+            short s => Equals(s),
+            byte b => Equals(b),
+            ulong ul => Equals((long)ul),
+            uint ui => Equals(ui),
+            ushort us => Equals(us),
             _ => false
         };
+
+    public readonly override string ToString() => _value.ToString();
 }
