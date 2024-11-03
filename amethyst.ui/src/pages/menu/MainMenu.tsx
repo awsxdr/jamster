692c460a34a3d8b76fddc49e8941c795a2b5f14a
Daniel Errington <derrington@gmail.com>
2024-11-02 21:36:04 -0700

    Persist sidebar open state

--- amethyst.ui/src/pages/menu/MainMenu.tsx
+++ amethyst.ui/src/pages/menu/MainMenu.tsx
@@ -3,16 +3,18 @@ import { MenuSidebar } from './components/MenuSidebar';
 import { ReactNode } from 'react';
 import styles from './MainMenu.module.scss';
 import { ThemeProvider } from '@/hooks/ThemeHook';
+import { getCookie } from 'typescript-cookie';
 
 type MainMenuProps = {
     content: ReactNode;
 };
 
 export const MainMenu = ({ content }: MainMenuProps) => {
+    const sidebarState = getCookie('sidebar:state') === 'true';
     return (
         <>
             <ThemeProvider defaultTheme='light'>
-                <SidebarProvider>
+                <SidebarProvider defaultOpen={sidebarState}>
                     <MenuSidebar />
                     <main className={styles.content}>
                         { content }
