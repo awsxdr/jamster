18cccf39224f5ff0be343b0f0ccfab0169ed0c46
Daniel Errington <derrington@gmail.com>
2024-12-31 21:54:04 +0000

    Add cross-platform start script

---
+++ start.cmd
@@ -0,0 +1,13 @@
+:<<"::CMDLITERAL"
+@ECHO OFF
+.\win-x64\amethyst %*
+exit /b 1
+::CMDLITERAL
+ARCH="$(uname -m)"
+if [ "$ARCH" = "aarch64" ]; then
+    ./linux-arm64/amethyst "$@"
+elif [ "$ARCH" = "x86_64" ]; then
+    ./linux-x64/amethyst "$@"
+else
+    echo "Unsupported architecture '$ARCH'"
+fi
