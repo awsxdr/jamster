7eef8b9cb3c597c68369ff9980d9a36543c66d81
Daniel Errington <derrington@gmail.com>
2025-01-27 22:14:53 +0000

    Move hotkeys configuration to user menu

--- amethyst.ui/src/pages/control/components/SettingsMenu.tsx
+++ amethyst.ui/src/pages/control/components/SettingsMenu.tsx
@@ -1,8 +1,7 @@
 import { Button, DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui";
 import { useCurrentGame, useI18n } from "@/hooks";
-import { History, Keyboard, List, Settings } from "lucide-react";
+import { History, List, Settings } from "lucide-react";
 import { Link } from "react-router-dom";
-import { ShortcutConfigurationDialog, ShortcutConfigurationDialogContainer, ShortcutConfigurationDialogTrigger } from "./ShortcutConfigurationDialog";
 
 type SettingsMenuProps = {
     disabled?: boolean;
@@ -15,39 +14,30 @@ export const SettingsMenu = ({ disabled }: SettingsMenuProps) => {
     const { translate } = useI18n();
 
     return (
-        <ShortcutConfigurationDialogContainer>
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button size="icon" variant="ghost" disabled={disabled}>
-                        <Settings />
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56">
-                    { currentGame && (
-                        <>
-                            <ShortcutConfigurationDialogTrigger>
-                                <DropdownMenuItem disabled={disabled}>
-                                    <Keyboard />
-                                    {translate("ScoreboardControl.SettingsMenu.Inputs")}
-                                </DropdownMenuItem>
-                            </ShortcutConfigurationDialogTrigger>
-                            <DropdownMenuItem disabled={disabled} asChild>
-                                <Link to={`/games/${currentGame?.id}`}>
-                                    <List />
-                                    {translate("ScoreboardControl.SettingsMenu.Settings")}
-                                </Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem disabled={disabled} asChild>
-                                <Link to={`/games/${currentGame?.id}/timeline`}>
-                                    <History />
-                                    {translate("ScoreboardControl.SettingsMenu.Timeline")}
-                                </Link>
-                            </DropdownMenuItem>
-                        </>
-                    )}
-                </DropdownMenuContent>
-            </DropdownMenu>
-            <ShortcutConfigurationDialog />
-        </ShortcutConfigurationDialogContainer>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button size="icon" variant="ghost" disabled={disabled}>
+                    <Settings />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                { currentGame && (
+                    <>
+                        <DropdownMenuItem disabled={disabled} asChild>
+                            <Link to={`/games/${currentGame?.id}`}>
+                                <List />
+                                {translate("ScoreboardControl.SettingsMenu.Settings")}
+                            </Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem disabled={disabled} asChild>
+                            <Link to={`/games/${currentGame?.id}/timeline`}>
+                                <History />
+                                {translate("ScoreboardControl.SettingsMenu.Timeline")}
+                            </Link>
+                        </DropdownMenuItem>
+                    </>
+                )}
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
--- amethyst.ui/src/pages/control/components/UserMenu.tsx
+++ amethyst.ui/src/pages/control/components/UserMenu.tsx
@@ -1,8 +1,9 @@
 import { Button, DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui";
 import { useI18n } from "@/hooks";
-import { LogOut, UserRound } from "lucide-react";
+import { Keyboard, LogOut, UserRound } from "lucide-react";
 import { LoginDialog, LoginDialogContainer, LoginDialogTrigger } from "./LoginDialog";
 import { useUserLogin } from "@/hooks/UserLogin";
+import { ShortcutConfigurationDialog, ShortcutConfigurationDialogContainer, ShortcutConfigurationDialogTrigger } from "./ShortcutConfigurationDialog";
 
 type UserMenuProps = {
     disabled?: boolean;
@@ -19,28 +20,38 @@ export const UserMenu = ({ disabled }: UserMenuProps) => {
 
     if (userName) {
         return (
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" disabled={disabled}>
-                        <UserRound />
-                        <span className="hidden lg:inline">
-                            {translate("UserMenu.Welcome").replace("{userName}", userName)}
-                        </span>
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56">
-                    <DropdownMenuGroup className="block lg:hidden">
-                        <DropdownMenuLabel>Hi, {userName}!</DropdownMenuLabel>
+            <ShortcutConfigurationDialogContainer>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" disabled={disabled}>
+                            <UserRound />
+                            <span className="hidden lg:inline">
+                                {translate("UserMenu.Welcome").replace("{userName}", userName)}
+                            </span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                        <DropdownMenuGroup className="block lg:hidden">
+                            <DropdownMenuLabel>Hi, {userName}!</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                        </DropdownMenuGroup>
+                        <ShortcutConfigurationDialogTrigger>
+                            <DropdownMenuItem disabled={disabled}>
+                                <Keyboard />
+                                {translate("ScoreboardControl.SettingsMenu.Inputs")}
+                            </DropdownMenuItem>
+                        </ShortcutConfigurationDialogTrigger>
                         <DropdownMenuSeparator />
-                    </DropdownMenuGroup>
-                    <DropdownMenuGroup>
-                        <DropdownMenuItem disabled={disabled} onClick={handleLogOut}>
-                            <LogOut />
-                            {translate("UserMenu.Logout")}
-                        </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                </DropdownMenuContent>
-            </DropdownMenu>
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem disabled={disabled} onClick={handleLogOut}>
+                                <LogOut />
+                                {translate("UserMenu.Logout")}
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                <ShortcutConfigurationDialog />
+            </ShortcutConfigurationDialogContainer>
         );
     } else {
         return (
