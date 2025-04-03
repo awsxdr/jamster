ecb6ab328df44d60c87e2645d4b37a81e3f1f453
Daniel Errington <derrington@gmail.com>
2025-04-03 18:41:47 +0100

    Wrap all routes in client activity tracking

--- amethyst.ui/src/routes.tsx
+++ amethyst.ui/src/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { CurrentGameScoreboard } from "./pages/scoreboard";
 import { MainMenu } from "./pages/menu";
 import { TitlePage } from "./pages/title";
@@ -12,76 +12,96 @@ import { Settings } from "./pages/settings";
 import { Timeline } from "./pages/timeline/Timeline";
 import { UsersManagement } from "./pages/users/UsersManagement";
 import { PenaltyLineup } from "./pages/penaltyLineup";
+import { createClientActivityRouter } from "./hooks";
+import { ClientActivity } from "./types";
+import { PenaltyWhiteboard } from "./pages/penaltyWhiteboard";
 
 export const Routes = () => {
-    const router = createBrowserRouter([
+    const router = createClientActivityRouter([
         {
             path: '/',
-            element: <MainMenu content={<TitlePage />} />
+            element: <MainMenu content={<TitlePage />} />,
+            activity: ClientActivity.Other,
         },
         {
             path: '/scoreboard',
-            element: <CurrentGameScoreboard />
+            element: <CurrentGameScoreboard />,
+            activity: ClientActivity.Scoreboard,
         },
         {
             path: '/sbo',
-            element: <MainMenu content={<ScoreboardControl />} />
+            element: <MainMenu content={<ScoreboardControl />} />,
+            activity: ClientActivity.ScoreboardOperator,
         },
         {
             path: '/teams',
             element: <MainMenu content={<TeamsManagement />} />,
+            activity: ClientActivity.Other,
         },
         {
             path: '/teams/:teamId',
             element: <MainMenu content={<TeamEdit />} />,
+            activity: ClientActivity.Other,
         },
         {
             path: '/games',
-            element: <MainMenu content={<GamesManagement />} />
+            element: <MainMenu content={<GamesManagement />} />,
+            activity: ClientActivity.Other,
         },
         {
             path: '/games/:gameId',
-            element: <MainMenu content={<GameEdit />} />
+            element: <MainMenu content={<GameEdit />} />,
+            activity: ClientActivity.Other,
         },
         {
             path: '/games/:gameId/rules',
-            element: <MainMenu content={<WorkInProgress />} />
+            element: <MainMenu content={<WorkInProgress />} />,
+            activity: ClientActivity.Other,
         },
         {
             path: '/games/:gameId/timeline',
-            element: <MainMenu content={<Timeline />} />
+            element: <MainMenu content={<Timeline />} />,
+            activity: ClientActivity.Other,
         },
         {
             path: '/help',
-            element: <MainMenu content={<WorkInProgress />} />
+            element: <MainMenu content={<WorkInProgress />} />,
+            activity: ClientActivity.Other,
         },
         {
             path: '/overlay',
-            element: <Overlay />
+            element: <Overlay />,
+            activity: ClientActivity.StreamOverlay,
         },
         {
             path: '/penalties',
-            element: <MainMenu content={<WorkInProgress />} />
+            element: <PenaltyWhiteboard />,
+            activity: ClientActivity.PenaltyWhiteboard,
         },
         {
             path: '/plt',
-            element: <MainMenu content={<PenaltyLineup />} />
+            element: <MainMenu content={<PenaltyLineup />} />,
+            activity: ClientActivity.PenaltyLineupControl,
         },
         {
             path: '/rulesets',
-            element: <MainMenu content={<WorkInProgress />} />
+            element: <MainMenu content={<WorkInProgress />} />,
+            activity: ClientActivity.Other,
         },
         {
             path: '/stats',
-            element: <MainMenu content={<WorkInProgress />} />
+            element: <MainMenu content={<WorkInProgress />} />,
+            activity: ClientActivity.Other,
         },
         {
             path: '/settings',
-            element: <MainMenu content={<Settings />} />
+            element: <MainMenu content={<Settings />} />,
+            activity: ClientActivity.Other,
         },
         {
             path: '/users',
-            element: <MainMenu content={<UsersManagement />} />
+            element: <MainMenu content={<UsersManagement />} />,
+            activity: ClientActivity.Other,
         },
     ]);
     
