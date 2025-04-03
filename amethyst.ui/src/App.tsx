36481b03e44a9817ea7e829365fce4d996814b45
Daniel Errington <derrington@gmail.com>
2025-04-03 18:41:07 +0100

    Add client connection context to app

--- amethyst.ui/src/App.tsx
+++ amethyst.ui/src/App.tsx
@@ -1,28 +1,30 @@
-import { ConfigurationContextProvider, GamesListContextProvider, SystemStateContextProvider, UserLoginContextProvider, UserSettingsContextProvider } from "@/hooks";
+import { ClientConnectionContextProvider, ConfigurationContextProvider, GamesListContextProvider, SystemStateContextProvider, UserLoginContextProvider, UserSettingsContextProvider } from "@/hooks";
 import { Routes } from "./routes";
 import { TeamListContextProvider } from "./hooks/TeamsHook";
 import { ShortcutsContextProvider } from "./hooks/InputControls";
 
 const App = () => {
-  return (
-    <>
-      <SystemStateContextProvider>
-        <GamesListContextProvider>
-          <TeamListContextProvider>
-            <ConfigurationContextProvider>
-              <UserLoginContextProvider>
-                <UserSettingsContextProvider>
-                  <ShortcutsContextProvider>
-                    <Routes />
-                  </ShortcutsContextProvider>
-                </UserSettingsContextProvider>
-              </UserLoginContextProvider>
-            </ConfigurationContextProvider>
-          </TeamListContextProvider>
-        </GamesListContextProvider>
-      </SystemStateContextProvider>
-    </>
-  )
+    return (
+        <>
+            <ClientConnectionContextProvider>
+                <SystemStateContextProvider>
+                    <GamesListContextProvider>
+                        <TeamListContextProvider>
+                            <ConfigurationContextProvider>
+                                <UserLoginContextProvider>
+                                    <UserSettingsContextProvider>
+                                        <ShortcutsContextProvider>
+                                            <Routes />
+                                        </ShortcutsContextProvider>
+                                    </UserSettingsContextProvider>
+                                </UserLoginContextProvider>
+                            </ConfigurationContextProvider>
+                        </TeamListContextProvider>
+                    </GamesListContextProvider>
+                </SystemStateContextProvider>
+            </ClientConnectionContextProvider>
+        </>
+    )
 }
 
 export default App
